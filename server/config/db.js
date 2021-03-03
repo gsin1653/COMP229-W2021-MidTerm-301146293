85e62c5cdf68852159df7d6aa7e4724cb73d39e6
Gagandeep Singh <gsin1653@my.centennialcollege.ca>
2021-03-03 19:27:05 +0530

    Added MongoDb Atlas URI for database connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://gagan08:gagan08@cluster0.0ahnh.mongodb.net/book_store?retryWrites=true&w=majority"
 };
