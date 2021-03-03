61a6ec41457d5aea911fb36fc6179a13356ecb43
Gagandeep Singh <gsin1653@my.centennialcollege.ca>
2021-03-03 19:16:30 +0530

    Fixed Add Button on Books Index page

--- server/routes/books.js
+++ server/routes/books.js
@@ -25,11 +25,7 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  res.render('books/details', { title: 'Add Book' });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
